a5c1e5fe8c88d834f709145bf10b3d78227cf725
= <hello@imsean.me>
2016-06-08 22:35:25 -0500

    Working on authorization flow

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,57 @@
 var Vue = require('vue');
+var VueResource = require('vue-resource');
 var VueRouter = require('vue-router');
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 Vue.component('navbar', require('./components/navbar.vue'));
 
-var Home = require('./pages/home.vue');
-
-var App = Vue.extend({});
+var App = Vue.extend({
+  http: {
+    root: '/api'
+  },
+  data: function() {
+    return {
+      loggedIn: false
+    };
+  },
+  ready: function() {
+    this.$root.data = this.data;
+  }
+});
 var Router = new VueRouter();
 
 Router.map({
   '/': {
-    component: Home
+    component: require('./pages/home.vue')
+  },
+  '/sign-in': {
+    component: require('./pages/sign-in.vue')
+  },
+  '/sign-up': {
+    component: require('./pages/sign-up.vue')
+  },
+  '/admin': {
+    component: require('./pages/admin/admin.vue'),
+    auth: true
+    // subRoutes: {
+    //   '/auth': {
+    //     component: require('./pages/admin/auth.vue')
+    //   }
+    // }
+  },
+  '*': {
+    component: require('./pages/not-found.vue')
   }
+});
+
+// Check for authentication here, this will be added later on
+Router.beforeEach(function(route) {
+  if (route.to.auth) {
+    return route.redirect('/auth/sign-in');
+  }
+  route.next();
 });
 
 /* global document */
